0fa832c627f8532fc8e25198cfeaef4b57afad26
SongChay <songchay.beng@student.uts.edu.au>
2018-05-28 00:16:30 +1000

    Commit

--- src/java/uts/wsd/Reserve.java
+++ src/java/uts/wsd/Reserve.java
@@ -3,37 +3,50 @@ package uts.wsd;
 import java.io.Serializable;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlAccessorType(XmlAccessType.FIELD)
-public class Reserve implements Serializable {
 
-	private String name;
-	private String email;
+public class Reserve implements Serializable {
+        
+        
+        private String rtitle;
+	private String rname;
+	private String remail;
 
 	public Reserve() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Reserve(String name, String email) {
+	public Reserve(String title, String name, String email) {
 		super();
-		this.name = name;
-		this.email = email;
+                this.rtitle= title;
+		this.rname = name;
+		this.remail = email;
 	}
 
+    public String getTitle() {
+        return rtitle;
+    }
+
+    public void setTitle(String title) {
+        this.rtitle = title;
+    }
+
 	public String getName() {
-		return name;
+		return rname;
 	}
 
 	public void setName(String name) {
-		this.name = name;
+		this.rname = name;
 	}
 
 	public String getEmail() {
-		return email;
+		return remail;
 	}
 
 	public void setEmail(String email) {
-		this.email = email;
+		this.remail = email;
 	}
 
 }
